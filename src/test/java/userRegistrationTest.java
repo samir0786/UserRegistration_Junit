c50b53fa7903bbb0028fe4311ca5c819096f93fc
samir0786 <samirsiescom@gmail.com>
2020-04-20 18:32:44 +0300

    ADDED: Validating Of User First Name

--- src/test/java/userRegistrationTest.java
+++ src/test/java/userRegistrationTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class userRegistrationTest{
@@ -5,5 +6,10 @@ public class userRegistrationTest{
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         boolean firstName = userRegistration.validInputName("Sameer");
 
+    }
+    @Test
+    public void givenFirstName_WhenNotInProperFormat_ShouldReturnFalse() {
+        boolean firstName = userRegistration.validInputName("sam010");
+        Assert.assertFalse(firstName);
     }
 }
